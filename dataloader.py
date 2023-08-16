532e4616d5745590bdf209e367820ae21aab5e7c
NavehRaz <NavehRaz@gmail.com>
2023-08-16 20:08:52 +0300

    Changes to be committed: 	modified:   dataloader.py
    Changes not staged for commit:
    	modified:   helper_classes.py

--- dataloader.py
+++ dataloader.py
@@ -2,7 +2,8 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import numpy as np
 import h5py
-import Project_DL.helper_classes as helper_classes
+from helper_classes import TF as TF
+from helper_classes import Chromosome as Chromosome 
 
 AA_nums = 20
 DBD_nums = 26
@@ -18,53 +19,55 @@ class TFDNA_ds(Dataset):
         #sliding_window_step - the step size for the sliding window, if 0 there is no sliding window
         #                       in which case there is no overlap between samples
         """
-
+        
         num_chroms =16
         # initialize TF array
         f_TF = h5py.File(TF_path, 'r')
         dset = f_TF['final_array']
-        self.num_TFs = len(dset)
-        self.TF_arr = np.array(self.num_TFs)
+        self.num_TFs = np.shape(dset)[0]
+        self.TF_arr = []
         for i in range(self.num_TFs):
-            self.TF_arr[i] = TF(dset[i])
+            self.TF_arr.append(TF(dset[i]))
         # TF array initialized
 
         # initialize DNA signal
-        self.chrom_lengths  =self.get_chrom_lengths(
-            num_chroms, file_name = DNA_path+'/DNA_data_fragments_')
-        self.num_ind = self.num_ind_per_chrom(self,self.chrom_lengths,seq_length,sliding_window_step)
         self.seq_length = seq_length
         self.sliding_window_step=sliding_window_step
-        self.chromosomes=[]
+        self.chrom_lengths  =self.get_chrom_lengths(
+            num_chroms=num_chroms, file_name = (DNA_path+'/DNA_data_fragments_'))
+        self.num_ind = self.num_ind_per_chrom(self.chrom_lengths,self.seq_length,self.sliding_window_step)
+        
+        self.chromosome_files=[]
         for i in range(num_chroms):
-            path = DNA_path+'/DNA_data_fragments_'+str(i+1)
+            path = DNA_path+'/DNA_data_fragments_'+str(i+1)+'.h5'
             file = h5py.File(path,'r')
-            chrom_key  = 'DNA_data_frags_001_chrom'+str(i+1)+'.h5'
-            chrom  = Chromosome(file[chrom_key],i+1)
-            self.chromosomes.append(chrom)
+            self.chromosome_files.append(file)
         # signal data initialized
 
     def __len__(self):
-        return np.sum(self.num_ind)
+        return int(np.sum(self.num_ind)*self.num_TFs)
 
     def __getitem__(self, idx):
         TF_num, chrom_num, loc_on_chrom = self.get_TF_chrom_and_loc_from_ind(
             idx,chrom_lengths=self.chrom_lengths,seq_length=self.seq_length,sliding_window_step=self.sliding_window_step)
-        chrom = self.chromosomes[chrom_num-1]
-        data_DNA, signal = chrom.getSequenceData(TF_num,loc_on_chrom)
-        data_TF = self.TF_arr[TF_num-1].getData()
-        data =(data_TF,data_DNA)
+        chrom_key  = self.getChromKey(TF_num, chrom_num)
+        chrom_file = self.chromosome_files[chrom_num-1]
+        chrom = Chromosome(
+            chrom_file[chrom_key],chrom_num,TF_num,seq_length=self.seq_length,sliding_window_step=self.sliding_window_step)
+        data_DNA, signal = chrom.getSequenceData(loc_on_chrom)
+        data_TF,DBD = self.TF_arr[TF_num-1].getData()
+        data =(data_TF,data_DNA ,DBD)
     
         return data, signal
 
-    def get_chrom_lengths(num_chroms =16, file_name = 'signal_159_TFs/DNA_data_fragments_'):
+    def get_chrom_lengths(self,num_chroms =16, file_name = 'signal_159_TFs/DNA_data_fragments_',TF=1):
         #returns an array witht the chromosomes lengths for indexing purposes
         chrom_lenghts  = np.zeros(num_chroms)
         for i in range(num_chroms):
-            path = file_name+str(i+1)
+            path = file_name+str(i+1)+'.h5'
             file = h5py.File(path,'r')
-            chrom_key  = 'DNA_data_frags_001_chrom'+str(i+1)+'.h5'
-            chrom  = Chromosome(file[chrom_key],i+1)
+            chrom_key  = 'DNA_data_frags_00'+str(TF)+'_chrom'+str(i+1)
+            chrom  = Chromosome(file[chrom_key],i+1,1)
             chrom_lenghts[i]=chrom.chrom_len
         return chrom_lenghts
 
@@ -88,6 +91,8 @@ class TFDNA_ds(Dataset):
 
 
     def num_ind_per_chrom(self,chrom_lengths,seq_length=300,sliding_window_step=1):
+        if sliding_window_step==0:
+            sliding_window_step = seq_length
         return ((chrom_lengths-seq_length)//sliding_window_step+1)
 
     def get_TF_chrom_and_loc_from_ind(self,ind,chrom_lengths,seq_length=300,sliding_window_step=1):
@@ -115,7 +120,15 @@ class TFDNA_ds(Dataset):
         chrom_num = np.argmax((cum_chrom_ind>loc_on_DNA))
         loc_on_chrom = loc_on_DNA-cum_chrom_ind[chrom_num-1]
 
-        return TF_num, chrom_num, loc_on_chrom
+        return int(TF_num), int(chrom_num), int(loc_on_chrom)
+    
+    def getChromKey(self,TF,chrom_num):
+        if TF<10:
+            return 'DNA_data_frags_00'+str(TF)+'_chrom'+str(chrom_num)
+        elif TF<100:
+            return 'DNA_data_frags_0'+str(TF)+'_chrom'+str(chrom_num)
+        else:
+            return 'DNA_data_frags_'+str(TF)+'_chrom'+str(chrom_num)
 
 def getKey(i):
     if i<10:
